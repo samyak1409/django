9543348c7ab69128cb5d527dbf4075d6f1ec05ae
Samyak Jain <samyak65400@gmail.com>
2023-01-20 20:28:06 +0530

    vid 3: render some demo data, using code in html

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -4,10 +4,31 @@ from django.shortcuts import render
 # Create your views here.
 
 
+posts = [
+    {
+        'title': 'Post 1',
+        'author': 'Samyak Jain',
+        'date': '13 January 2023',
+        'content': 'This is my first blog post.'
+    },
+    {
+        'title': 'Post 2',
+        'author': 'Samyak Jain',
+        'date': '13 January 2023',
+        'content': 'This is my second blog post.'
+    }
+]  # let's just pretend for now that we made a database call and got back this list of posts, and we want to display
+# these posts on our blog homepage
+
+
 def home(request):
-    return render(request=request, template_name='blog/home.html')
+    context = {
+        'posts': posts,
+        'title': 'Home'
+    }
+    return render(request=request, template_name='blog/home.html', context=context)
     # views always need to return either an `HttpResponse` or an `Exception`
 
 
 def about(request):
-    return render(request=request, template_name='blog/about.html')
+    return render(request=request, template_name='blog/about.html', context={'title': 'About'})
