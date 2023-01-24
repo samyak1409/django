97f896289ff1d069d5a00bab8aee69aa41ca1486
Samyak Jain <samyak65400@gmail.com>
2023-01-24 17:21:10 +0530

    vid 5: added the data source to views, fixed date formatting

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,29 +1,12 @@
 from django.shortcuts import render
-
+from .models import Post
 
 # Create your views here.
 
 
-posts = [
-    {
-        'title': 'Post 1',
-        'author': 'Samyak Jain',
-        'date': '13 January 2023',
-        'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque eligendi excepturi quam quia, repellendus saepe similique. Accusamus amet aperiam aspernatur, assumenda consequuntur delectus dolor excepturi iure labore molestiae mollitia nihil nobis officia optio perferendis quaerat, repellat, repellendus sequi sunt tempora veniam vero voluptates! Accusantium libero, maiores minima mollitia obcaecati quo similique ut vitae! Alias aliquam animi aperiam aspernatur consequuntur corporis culpa dicta eius esse eveniet explicabo facilis, hic ipsa itaque magni maxime molestias mollitia natus necessitatibus nisi numquam odio perferendis porro praesentium quaerat qui quidem quod reiciendis repudiandae sequi suscipit temporibus tenetur totam unde velit voluptates voluptatum. Quam, vel, voluptatum.'
-    },
-    {
-        'title': 'Post 2',
-        'author': 'Samyak Jain',
-        'date': '13 January 2023',
-        'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquam aut blanditiis consequuntur culpa cumque dolorum eius est et facere facilis harum ipsa ipsam ipsum laudantium libero magni maiores minima modi nesciunt numquam odio officiis perferendis perspiciatis porro praesentium quas ratione reprehenderit rerum sint soluta tempora tempore, vel veniam voluptatum.'
-    }
-]  # let's just pretend for now that we made a database call and got back this list of posts, and we want to display
-# these posts on our blog homepage
-
-
 def home(request):
     context = {
-        'posts': posts,
+        'posts': Post.objects.all(),
         'title': 'Home'
     }
     return render(request=request, template_name='blog/home.html', context=context)
