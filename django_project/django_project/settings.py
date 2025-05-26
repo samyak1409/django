16417c35bea4cd5ec1e2adbdaae530b6f6502600
Samyak Jain <samyak65400@gmail.com>
2025-05-27 04:23:50 +0530

    add whitenoise

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -145,7 +145,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # where collectstatic will gather all static content
-
+# WhiteNoise for serving static files in production
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')  # right after SecurityMiddleware
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  # for compression and caching
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
