092c294ee3963c586d97bfe91bcf401c6cc1be54
Samyak Jain <samyak65400@gmail.com>
2025-06-06 21:10:02 +0530

    add API link in header; improve responsiveness on profile page; delete collectstatic files; add .py-ver file for koyeb

--- django_project/api1/urls.py
+++ django_project/api1/urls.py
@@ -3,7 +3,7 @@ from .views import available_endpoints, all_posts, single_post, posts_by_a_user
 
 
 urlpatterns = [
-    path('', available_endpoints),
+    path('', available_endpoints, name='api1'),
     path('posts/', all_posts),
     path('posts/<int:pk>/', single_post),
     path('posts/by/<str:username>/', posts_by_a_user),
